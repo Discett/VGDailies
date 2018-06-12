3779688c4078b6a85cfefae2dc64afdb4be996d6
Gman <gabemink@gmail.com>
2018-06-12 12:28:31 -0700

    creating user model

--- mysite/homepage/models.py
+++ mysite/homepage/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class user(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+
+    #has built in email validator
+    email =  models.EmailField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.username
