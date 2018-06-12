645f9850891830535c0134fe2764be856a25bf0e
Gman <gabemink@gmail.com>
2018-06-12 12:13:47 -0700

    spring cleaning

--- mysite/admin/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def index(request):
-    #needed for each view, represents content of page loaded
-    return HttpResponse("")
