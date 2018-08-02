bad9ed76185bfbb237d911a2e241ebae46b36582
Randy <randyluong909@gmail.com>
2018-08-01 19:56:34 -0700

    update to auth, still missing many views, not functional

--- dailiesBackend/dailiesBackend/settings.py
+++ dailiesBackend/dailiesBackend/settings.py
@@ -32,9 +32,20 @@ ALLOWED_HOSTS = []
 
 CORS_ORIGIN_ALLOW_ALL = True
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
+
 INSTALLED_APPS = [
     'corsheaders',
-    'login.apps.LoginConfig',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djangoEndPoints.apps.DjangoendpointsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
